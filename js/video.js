e838d65c4decd8805a3be335c34057bd7c4b0065
Mads <90150746+maddjenn@users.noreply.github.com>
2023-03-30 23:36:40 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -4,9 +4,8 @@ var video = document.querySelector('.video');
 // sets video variables when we open our window
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-	video = document.querySelector('#player1');
-	video.setAttribute("autoplay", "false")
-	video.removeAttribute("loop")
+	video.autoplay = false;
+	video.loop = false;
 	video.load();
 });
 
